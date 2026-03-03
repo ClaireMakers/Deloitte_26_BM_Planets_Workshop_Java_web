ab63a6d2b4100f62c58e93598748a941e219b219
Claire Castanet <clairecastanet@Mac.lan>
2026-03-03 10:50:14 +0000

    Day 2 workshop code

--- src/main/java/com/makers/makersbnb_coach_support_code/controller/PlanetsController.java
+++ src/main/java/com/makers/makersbnb_coach_support_code/controller/PlanetsController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+//Server-side rendering - we are responding with whole templates
 @Controller
 public class PlanetsController {
     // tell Spring Boot this method handles the 'GET "/"' request
@@ -11,4 +12,17 @@ public class PlanetsController {
     public ModelAndView planetsList() {
         return new ModelAndView("/PlanetsList");
     }
+
+    @GetMapping("/mars")
+    public ModelAndView getMarsInfo() {
+        ModelAndView marsInfoTemplate = new ModelAndView("/MarsInfo");
+        String marsDescription = "The planet Mars is half the size of the Earth";
+
+        marsInfoTemplate.addObject("marsDescription", marsDescription);
+        return marsInfoTemplate;
+    }
 }
+
+//@RestController -> to build servers that respond with purely data in JSON format or similar
+
+
